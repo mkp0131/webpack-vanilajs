30c5254564f3e6a7593728b1de3e4d43f41e183a
test <test>
2022-07-12 23:36:36 +0900

    기본세팅

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import { sum } from './math';
+
+console.log('7 + 5 = ', sum(7, 5));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  // devtool: 'source-map',
+};
